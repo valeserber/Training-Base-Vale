52ab4d34cfe29e53868992d9420960a3e92dfb25
valeserber <valserber@gmail.com>
2015-01-20 13:12:40 -0300

    profile layout

--- app/src/main/java/com/example/valeriaserber/trainingapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/valeriaserber/trainingapp/fragments/ProfileFragment.java
@@ -8,14 +8,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.valeriaserber.trainingapp.R;
+import com.example.valeriaserber.trainingapp.model.SessionObject;
+import com.squareup.picasso.Picasso;
 
 public class ProfileFragment extends Fragment {
-
-    public ProfileFragment() {
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
